0d12c8b88e7658568f809f60e02410afbec9df02
l0stg <maxim99112@gmail.com>
2022-06-23 22:23:56 +0500

    sqr calc

--- app/src/main/java/com/example/helloworld/SqrCalc.kt
+++ app/src/main/java/com/example/helloworld/SqrCalc.kt
@@ -5,19 +5,36 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.lang.Math.sqrt
 
 class SqrCalc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sqr_calc)
         val button_calc_x = findViewById<Button>(R.id.calc_x)
-        button_calc_x.setOnClickListener{
 
-                val a: Int = findViewById<EditText>(R.id.editA).text.toString().toInt()
-                val b: Int = findViewById<EditText>(R.id.editB).text.toString().toInt()
-                val c: Int = findViewById<EditText>(R.id.editC).text.toString().toInt()
+        fun descriminantCalc(a:Double, b: Double, c: Double): Double {
+            var descriminant: Double = b*b - 4*a*c
+            return descriminant
+        }
+
+        fun x_one(a:Double, b: Double, c: Double): Double {
+            val x1 =  (-b + kotlin.math.sqrt(descriminantCalc(a, b, c))) / (2 * a)
+            return (x1)
+        }
+        fun x_second(a:Double, b: Double, c: Double): Double {
+            val x2 =  (-b - kotlin.math.sqrt(descriminantCalc(a, b, c))) / (2 * a)
+            return (x2)
+        }
+
+        button_calc_x.setOnClickListener{
+                val a: Double = findViewById<EditText>(R.id.editA).text.toString().toDouble()
+                val b: Double = findViewById<EditText>(R.id.editB).text.toString().toDouble()
+                val c: Double = findViewById<EditText>(R.id.editC).text.toString().toDouble()
+                val xone = x_one(a, b, c).toString()
+                val xsecond = x_second(a, b, c).toString()
                 val result = findViewById<TextView>(R.id.tvresults)
-                result.text = (a*b*c).toString()
+                result.text = "x1= $xone и x2= $xsecond"
 
         }
     }
