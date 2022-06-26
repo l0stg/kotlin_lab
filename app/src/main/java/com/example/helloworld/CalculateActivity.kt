0942c382bd244c7e5bd94872d415333f6ed3d832
l0stg <maxim99112@gmail.com>
2022-06-26 23:16:02 +0500

    delete comments

--- app/src/main/java/com/example/helloworld/CalculateActivity.kt
+++ app/src/main/java/com/example/helloworld/CalculateActivity.kt
@@ -2,7 +2,7 @@ package com.example.finalcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.helloworld.R
-//import kotlinx.android.synthetic.main.activity_main.*
+
 
 
 class CalculateActivity : AppCompatActivity() {
